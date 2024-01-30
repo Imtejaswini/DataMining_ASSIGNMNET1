039a3af62968b653684b475fdee176ef10185abd
Imtejaswini <70140522+Imtejaswini@users.noreply.github.com>
2024-01-29 20:54:41 -0500

    Add files via upload

---
+++ question7.py
@@ -0,0 +1,14 @@
+def question7_1():
+    answer ='increase/decrease'
+    return answer 
+
+def question7_2():
+    answer = 'increase/decrease'
+    return answer
+
+def question7_3():
+    answer = [('infinity', 0.),  
+              (1., 3.),              
+              (4., 5.),              
+              (6., 'infinity')]
+    return answer
